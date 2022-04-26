4a51cddaba7b54c0c8f97bda1efe570be98486a0
Leo Wong <leoelicos@gmail.com>
2022-04-26 23:56:22 +1000

    Simplified test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,27 +1,36 @@
 const Employee = require('../lib/employee');
 
 describe('Employee class', () => {
+	const name = 'Jenny';
+	const id = 2;
+	const email = 'jenny@hotmail.com';
+	const employee = new Employee(name, id, email);
+	const returnedName = employee.getName();
+	const returnedId = employee.getId();
+	const returnedEmail = employee.getEmail();
+	const returnedRole = employee.getRole();
+
 	describe('getName', () => {
 		it('should return employee name', () => {
-			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getName()).toBe('Jenny');
+			expect(returnedName).toBe('Jenny');
 		});
 	});
 
 	describe('getId', () => {
 		it('should return id', () => {
-			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getId()).toBe(2);
+			expect(returnedId).toBe(2);
 		});
 	});
 
 	describe('getEmail', () => {
 		it('should return email', () => {
-			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getEmail()).toBe('jenny@hotmail.com');
+			expect(returnedEmail).toBe('jenny@hotmail.com');
 		});
 	});
 
 	describe('getRole', () => {
 		it("should return 'Employee'", () => {
-			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getRole()).toBe('Employee');
+			expect(returnedRole).toBe('Employee');
 		});
 	});
 });
