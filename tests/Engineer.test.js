a6974b158a31011548003e27a58693ba1c868e97
Leo Wong <leoelicos@gmail.com>
2022-04-27 00:09:54 +1000

    Add Engineer Tests

---
+++ tests/Engineer.test.js
@@ -0,0 +1,69 @@
+const Engineer = require('../lib/engineer');
+const Employee = require('../lib/employee');
+
+describe('Engineer class', () => {
+	const engineer = new Engineer('Jenny', 2, 'jenny@hotmail.com', 'jenny22');
+	const name = engineer.name;
+	const id = engineer.id;
+	const email = engineer.email;
+	const github = engineer.github;
+	const returnedName = engineer.getName();
+	const returnedId = engineer.getId();
+	const returnedEmail = engineer.getEmail();
+	const returnedRole = engineer.getRole();
+	const returnedGithub = engineer.getGithub();
+
+	describe('name', () => {
+		it('should store engineer name', () => {
+			expect(name).toBe('Jenny');
+		});
+	});
+
+	describe('id', () => {
+		it('should store engineer id', () => {
+			expect(id).toBe(2);
+		});
+	});
+
+	describe('email', () => {
+		it('should store engineer email', () => {
+			expect(email).toBe('jenny@hotmail.com');
+		});
+	});
+
+	describe('github', () => {
+		it('should store engineer github', () => {
+			expect(github).toBe('jenny22');
+		});
+	});
+
+	describe('getName', () => {
+		it('should return engineer name', () => {
+			expect(returnedName).toBe('Jenny');
+		});
+	});
+
+	describe('getId', () => {
+		it('should return engineer id', () => {
+			expect(returnedId).toBe(2);
+		});
+	});
+
+	describe('getEmail', () => {
+		it('should return engineer email', () => {
+			expect(returnedEmail).toBe('jenny@hotmail.com');
+		});
+	});
+
+	describe('getRole', () => {
+		it("should return 'Engineer'", () => {
+			expect(returnedRole).toBe('Engineer');
+		});
+	});
+
+	describe('getGitHub', () => {
+		it('should return github username', () => {
+			expect(returnedGithub).toBe('jenny22');
+		});
+	});
+});
