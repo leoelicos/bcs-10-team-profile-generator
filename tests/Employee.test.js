f5c40a46ef7328fb81e9cfd871467b84845d7d79
Leo Wong <leoelicos@gmail.com>
2022-04-26 23:52:20 +1000

    Update test wording

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,25 +1,25 @@
 const Employee = require('../lib/employee');
 
-describe('employee.js', () => {
-	describe('getName()', () => {
+describe('Employee class', () => {
+	describe('getName', () => {
 		it('should return employee name', () => {
 			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getName()).toBe('Jenny');
 		});
 	});
 
-	describe('getId()', () => {
+	describe('getId', () => {
 		it('should return id', () => {
 			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getId()).toBe(2);
 		});
 	});
 
-	describe('getEmail()', () => {
+	describe('getEmail', () => {
 		it('should return email', () => {
 			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getEmail()).toBe('jenny@hotmail.com');
 		});
 	});
 
-	describe('getRole()', () => {
+	describe('getRole', () => {
 		it("should return 'Employee'", () => {
 			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getRole()).toBe('Employee');
 		});
