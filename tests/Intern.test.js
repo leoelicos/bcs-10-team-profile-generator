beee652484c9ede5a4570e9e5fcec58ea6948804
Leo Wong <leoelicos@gmail.com>
2022-04-27 00:12:24 +1000

    Added tests for Intern.js

---
+++ tests/Intern.test.js
@@ -0,0 +1,69 @@
+const Intern = require('../lib/intern');
+const Employee = require('../lib/employee');
+
+describe('Intern class', () => {
+	const intern = new Intern('Jenny', 2, 'jenny@hotmail.com', 'St Clare High School');
+	const name = intern.name;
+	const id = intern.id;
+	const email = intern.email;
+	const school = intern.school;
+	const returnedName = intern.getName();
+	const returnedId = intern.getId();
+	const returnedEmail = intern.getEmail();
+	const returnedRole = intern.getRole();
+	const returnedSchool = intern.getSchool();
+
+	describe('name', () => {
+		it('should store intern name', () => {
+			expect(name).toBe('Jenny');
+		});
+	});
+
+	describe('id', () => {
+		it('should store intern id', () => {
+			expect(id).toBe(2);
+		});
+	});
+
+	describe('email', () => {
+		it('should store intern email', () => {
+			expect(email).toBe('jenny@hotmail.com');
+		});
+	});
+
+	describe('school', () => {
+		it('should store intern school', () => {
+			expect(school).toBe('St Clare High School');
+		});
+	});
+
+	describe('getName', () => {
+		it('should return intern name', () => {
+			expect(returnedName).toBe('Jenny');
+		});
+	});
+
+	describe('getId', () => {
+		it('should return intern id', () => {
+			expect(returnedId).toBe(2);
+		});
+	});
+
+	describe('getEmail', () => {
+		it('should return intern email', () => {
+			expect(returnedEmail).toBe('jenny@hotmail.com');
+		});
+	});
+
+	describe('getRole', () => {
+		it("should return 'Intern'", () => {
+			expect(returnedRole).toBe('Intern');
+		});
+	});
+
+	describe('getSchool', () => {
+		it('should return name of school', () => {
+			expect(returnedSchool).toBe('St Clare High School');
+		});
+	});
+});
