1b7e7f6e92652f23259cc13b725afa2f883e99b7
Leo Wong <leoelicos@gmail.com>
2022-04-26 23:47:46 +1000

    Added basic tests

---
+++ tests/Employee.test.js
@@ -0,0 +1,27 @@
+const Letter = require('../lib/employee');
+
+describe('employee.js', () => {
+	describe('getName()', () => {
+		it('should return employee name', () => {
+			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getName()).toBe('Jenny');
+		});
+	});
+
+	describe('getId()', () => {
+		it('should return id', () => {
+			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getId()).toBe(2);
+		});
+	});
+
+	describe('getEmail()', () => {
+		it('should return email', () => {
+			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getEmail()).toBe('jenny@hotmail.com');
+		});
+	});
+
+	describe('getRole()', () => {
+		it("should return 'Employee'", () => {
+			expect(new Employee('Jenny', 2, 'jenny@hotmail.com').getRole()).toBe('Employee');
+		});
+	});
+});
