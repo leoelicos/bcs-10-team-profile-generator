9216edd5bc9509bf99c19d9650e25b6ba463b814
Leo Wong <leoelicos@gmail.com>
2022-04-27 20:42:54 +1000

    Replace test string literals with const

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,58 +1,77 @@
 const Employee = require('../lib/employee');
+const testName = 'Jenny';
+const testId = 2;
+const testEmail = 'jenny@hotmail.com';
 
 describe('Employee class', () => {
 	describe('Initialization', () => {
 		it("should return an object containing a 'name' property when called with the 'new' keyword", () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
+			const employee = new Employee(testName, testId, testEmail);
 			expect('name' in employee).toEqual(true);
 		});
 		it("should set 'name' when created", () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
-			expect(employee.name).toBe('Jenny');
+			const employeeName = new Employee(testName, testId, testEmail).name;
+			expect(employeeName).toBe(testName);
 		});
 		it("should return an object containing an 'id' property when called with the 'new' keyword", () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
+			const employee = new Employee(testName, testId, testEmail);
 			expect('id' in employee).toEqual(true);
 		});
 		it("should set 'id' when created", () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
-			expect(employee.id).toBe(2);
+			const employeeId = new Employee(testName, testId, testEmail).id;
+			expect(employeeId).toBe(testId);
 		});
 		it("should return an object containing an 'email' property when called with the 'new' keyword", () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
+			const employee = new Employee(testName, testId, testEmail);
 			expect('email' in employee).toEqual(true);
 		});
 		it("should set 'email' when created", () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
-			expect(employee.email).toBe('jenny@hotmail.com');
+			const employeeEmail = new Employee(testName, testId, testEmail).email;
+			expect(employeeEmail).toBe(testEmail);
 		});
 	});
 
 	describe('getName', () => {
+		it('should return a string', () => {
+			const str = new Employee(testName, testId, testEmail).getName();
+			expect(typeof str).toBe('string');
+		});
 		it('should return employee name', () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
-			expect(employee.getName()).toBe('Jenny');
+			const employee = new Employee(testName, testId, testEmail);
+			expect(employee.getName()).toBe(testName);
 		});
 	});
 
 	describe('getId', () => {
+		it('should return a string', () => {
+			const str = new Employee(testName, testId, testEmail).getId();
+			expect(typeof str).toBe('string');
+		});
 		it('should return id', () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
-			expect(employee.getId()).toBe(2);
+			const employee = new Employee(testName, testId, testEmail);
+			expect(employee.getId()).toBe(testId);
 		});
 	});
 
 	describe('getEmail', () => {
+		it('should return a string', () => {
+			const str = new Employee(testName, testId, testEmail).getEmail();
+			expect(typeof str).toBe('string');
+		});
 		it('should return email', () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
-			expect(employee.getEmail()).toBe('jenny@hotmail.com');
+			const str = new Employee(testName, testId, testEmail).getEmail();
+			expect(str).toBe(testEmail);
 		});
 	});
 
 	describe('getRole', () => {
+		it('should return a string', () => {
+			const str = new Employee(testName, testId, testEmail).getRole();
+			expect(typeof str).toBe('string');
+		});
 		it("should return 'Employee'", () => {
-			const employee = new Employee('Jenny', 2, 'jenny@hotmail.com');
-			expect(employee.getRole()).toBe('Employee');
+			const str = new Employee(testName, testId, testEmail).getRole();
+			expect(str).toBe('Employee');
 		});
 	});
 });
