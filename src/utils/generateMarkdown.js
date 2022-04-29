37d3145ed179802719bbdbe400599be02b10866a
Leo Wong <leoelicos@gmail.com>
2022-04-30 09:46:01 +1000

    generate dynamic elements

--- src/utils/generateMarkdown.js
+++ src/utils/generateMarkdown.js
@@ -7,6 +7,16 @@ const ROLE_INTERN = 2;
 
 // a function to generate markdown for HTML
 function generateMarkdown(data) {
+	//mock data for testing
+	data = [
+		{ name: 'Jared', id: '1', email: 'jared@fakemail.com', details: '1', menu: 'Engineer', role: 0 },
+		{ name: 'Alec', id: '2', email: 'alec@fakemail.com', details: 'ibealec', menu: 'Engineer', role: 1 },
+		{ name: 'Grace', id: '3', email: 'grace@fakemail.com', details: 'gchoi2u', menu: 'Engineer', role: 1 },
+		{ name: 'Tammer', id: '4', email: 'tammer@fakemail.com', details: 'tammerg', menu: 'Intern', role: 1 },
+		{ name: 'John', id: '5', email: 'john@fakemail.com', details: '2University', menu: 'No more', role: 2 },
+	];
+
+	// raw template
 	const ROLE_MANAGER = 0;
 	const ROLE_ENGINEER = 1;
 	const ROLE_INTERN = 2;
@@ -15,7 +25,7 @@ function generateMarkdown(data) {
 	const projectTitle = 'Team Profile Generator';
 	const headerText = 'My Team';
 
-	const html = `<!DOCTYPE html>
+	let html = `<!DOCTYPE html>
 	<html lang="en">
 		<head>
 			<meta charset="UTF-8" />
