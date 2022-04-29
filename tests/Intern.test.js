1a217ff88dbc3b4c7d7cbddb47eae1daf9725971
Leo Wong <leoelicos@gmail.com>
2022-04-29 19:22:17 +1000

    Implement Intern Tests

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,75 +1,52 @@
-describe('dummy', () => {
-	it('should pass', () => {
-		expect(1).toBe(1);
-	});
-});
-/* const Intern = require('../lib/Intern');
-const Employee = require('../lib/Employee');
-
+const Intern = require('../lib/Intern');
+/* 
+school
+getSchool()
+getRole()—overridden to return 'Intern'
+*/
 describe('Intern class', () => {
-	const intern = new Intern('Jenny', 2, 'jenny@hotmail.com', 'St Clare High School');
-	const name = intern.name;
-	const id = intern.id;
-	const email = intern.email;
-	const school = intern.school;
-	const returnedName = intern.getName();
-	const returnedId = intern.getId();
-	const returnedEmail = intern.getEmail();
-	const returnedRole = intern.getRole();
-	const returnedSchool = intern.getSchool();
-
-	describe('name', () => {
-		it('should store intern name', () => {
-			expect(name).toBe('Jenny');
-		});
-	});
-
-	describe('id', () => {
-		it('should store intern id', () => {
-			expect(id).toBe(2);
-		});
-	});
-
-	describe('email', () => {
-		it('should store intern email', () => {
-			expect(email).toBe('jenny@hotmail.com');
-		});
-	});
-
-	describe('school', () => {
-		it('should store intern school', () => {
-			expect(school).toBe('St Clare High School');
-		});
-	});
-
-	describe('getName', () => {
-		it('should return intern name', () => {
-			expect(returnedName).toBe('Jenny');
-		});
-	});
-
-	describe('getId', () => {
-		it('should return intern id', () => {
-			expect(returnedId).toBe(2);
-		});
-	});
-
-	describe('getEmail', () => {
-		it('should return intern email', () => {
-			expect(returnedEmail).toBe('jenny@hotmail.com');
-		});
-	});
-
-	describe('getRole', () => {
-		it("should return 'Intern'", () => {
-			expect(returnedRole).toBe('Intern');
+	describe('initialization', () => {
+		// positive test
+		it("returns object with 'school' property set to 'school' argument when called with the 'new' keyword", () => {
+			const validName = 'Jenny';
+			const validId = 2;
+			const validEmail = 'jenny@hotmail.com';
+			const school = 'Harvard University';
+			const intern = new Intern(validName, validId, validEmail, school);
+			expect(intern.school).toBe(school);
 		});
 	});
 
 	describe('getSchool', () => {
-		it('should return name of school', () => {
-			expect(returnedSchool).toBe('St Clare High School');
+		// positive test
+		it("returns intern's school", () => {
+			// arrange
+			const validName = 'Jenny';
+			const validId = 2;
+			const validEmail = 'jenny@hotmail.com';
+			const school = 'Harvard University';
+			const intern = new Intern(validName, validId, validEmail, school);
+			const result = intern.getSchool();
+
+			// assert
+			expect(result).toBe(school);
+		});
+	});
+
+	describe('getRole', () => {
+		// positive test
+		it("returns role of 'Intern'", () => {
+			// arrange
+			const role = 'Intern';
+			const validName = 'Jenny';
+			const validId = 2;
+			const validEmail = 'jenny@hotmail.com';
+			const school = 'Harvard University';
+			const intern = new Intern(validName, validId, validEmail, school);
+			const result = intern.getRole();
+
+			// assert
+			expect(result).toBe(role);
 		});
 	});
 });
- */
