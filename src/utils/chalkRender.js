2816f780026408ea9c830ac5dc9b524be149e0e6
Leo Wong <leoelicos@gmail.com>
2022-04-30 21:19:58 +1000

    modularize chalk utility

---
+++ src/utils/chalkRender.js
@@ -0,0 +1,17 @@
+/* 
+ chalkRender.js
+ 
+ this file contains code to render messages in the console using customization via chalk
+
+ Copyright Leo Wong 2022
+ */
+
+const chalk = require('chalk');
+
+// chalk callback functions add color to console logs for visual impact
+const primary = (w) => chalk.magenta.bgWhite(` ${w} `);
+const secondary = (w) => chalk.greenBright.bgGray(` ${w} `);
+const tertiary = (w) => chalk.black.bgWhite(` ${w} `);
+const greeting = (w) => console.log(tertiary(w));
+
+module.exports = { primary, secondary, tertiary, greeting };
