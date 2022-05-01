c4bccba414116a3d28b03efc135442a584d9a8c4
Leo Wong <leoelicos@gmail.com>
2022-05-01 11:34:11 +1000

    Change employees[] to const, rename getMember to getEmployees

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ Copyright Leo Wong 2022
 const copyFile = require('./src/utils/copyFile');
 
 // utility function to create new classes based on user input
-const getMember = require('./src/utils/getMember.js');
+const getEmployees = require('./src/utils/getEmployees.js');
 
 // utility function to create good-looking console logs
 const chalkRender = require('./src/utils/chalkRender');
@@ -28,21 +28,21 @@ const htmlDestination = './dist/myTeam.html';
 // function to initialize app
 async function init() {
 	// array to store all of the team members
-	let employees = [];
+	const employees = [];
 
 	// greet user
 	chalkRender.greeting(`Welcome to Team Profile Generator! Let's do this!`);
 
-	// ask user questions about team manager and any other members
-	await getMember(employees, 'Manager');
+	// recursively prompt user for employee info and update employees[], starting with 'Manager'
+	await getEmployees(employees, 'Manager');
 
-	// copy normalize.css file from utils/ to dist/
+	// copy normalize.css file from Assets/css/ to dist/
 	await copyFile(normalizeSource, normalizeDestination);
 
-	// copy style.css file from utils/ to dist/
+	// copy style.css file from Assets/css/ to dist/
 	await copyFile(styleSource, styleDestination);
 
-	// write the HTML file based on the information obtained from getMember function on line 38
+	// write the HTML file based on the information obtained from getEmployees function on line 37
 	await writeHTML(htmlDestination, employees);
 }
 
