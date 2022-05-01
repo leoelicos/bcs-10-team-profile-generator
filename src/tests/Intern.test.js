e037b4a6423f0ffea3ffce51a6de769d8155014b
Leo Wong <leoelicos@gmail.com>
2022-05-01 12:11:19 +1000

    Adjust test message

--- src/tests/Intern.test.js
+++ src/tests/Intern.test.js
@@ -19,7 +19,7 @@ describe('Intern class', () => {
 
 	describe('getSchool', () => {
 		// positive test
-		it("returns 'Harvard University' when Intern is called with the new keyword and 'school' param is 'Harvard University'", () => {
+		it("returns 'Harvard University' when Intern is called with the 'new' keyword and 'school' param is 'Harvard University'", () => {
 			// arrange
 			const validName = 'Jenny';
 			const validId = 2;
@@ -35,7 +35,7 @@ describe('Intern class', () => {
 
 	describe('getRole', () => {
 		// positive test
-		it("returns 'Intern' when Intern is called with the new keyword", () => {
+		it("returns 'Intern' when Intern is called with the 'new' keyword", () => {
 			// arrange
 			const role = 'Intern';
 			const validName = 'Jenny';
